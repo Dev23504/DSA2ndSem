ce1c0389a81bbfc1bd95d3092b1e5ad863fd461f
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-02-24 10:46:52 +0530

    Notes

--- 2_ROW_SYM/Col_Asym.cpp
+++ 2_ROW_SYM/Col_Asym.cpp
@@ -13,25 +13,22 @@ int main()
         {
             cout << "* "; // a loop
         }
-        for (int j = 1; j <= n - i; j++)
+        for (int j = 1; j <= 2*(n - i)-1; j++)
         {
             cout << "  "; // b loop
         }
 
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << "  "; // b loop
-        }
-        if (i != n)
-            for (int j = 1; j <= i; j++)
+       
+        if (i == n)
+            for (int j = 1; j <= n-1; j++)
             {
                 cout << "* "; // a loop
             }
         else
         {
-            for (int j = 1; j <= i - 1; j++)
+            for (int j = 1; j <= i ; j++)
             {
-                cout << "  ";
+                cout << "* ";
             }
         }
     }
