decacea5928402ab69eaaa10cd1e7b149fab7b5c
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-02-24 11:07:27 +0530

    Asym

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,37 +3,15 @@ using namespace std;
 
 int main() {
     int n;
-    int m=(n+1)/2;
     cin>>n;
-    for (int i=1; i <=m; i++) {
+    for (int i=1; i <=n; i++) {
         cout<<"\n";
         // space
-        for (int j=1; j<=m-i; j++){
+        for (int j=1; j<=n-i; j++){
         cout << "  ";
         }
         for (int j=1; j<=2*i-1; j++){
-        if (j%2!=0)
-        {
-           cout << "* ";
-        }
-        else
-        cout << "  ";
-        }
-    }
-    cout << endl; 
-    for (int i=1; i >=n; i--) {
-        cout<<"\n";
-        // space
-        for (int j=1; j<=i; j++){
-        cout << "  ";
-        }
-        for (int j=1; j<=2*i-1; j++){
-        if (j%2!=0)
-        {
-           cout << "* ";
-        }
-        else
-        cout << "  ";
+        cout << "* ";
         }
     }
     cout << endl; 
