7ea2ad8cca81dc16cb71c44ea850bd6951fc8133
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-02-26 11:46:45 +0530

    notes

---
+++ 2_ROW_SYM/Lec_8_p_5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        // space
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        // space
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+ 
+
+//         * * * * * 
+//       * * * *
+//     * * *
+//   * *
+// *
+//   * *
+//     * * *
+//       * * * *
+//         * * * * *
---
+++ 2_ROW_SYM/Lecture_9_p_4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int cnt = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "\n";
+        // space left
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = i; j <= 2 * i - 1; j++)
+        {
+            cout << j << " ";
+        }
+        // space right
+        for (int j = 2 * i - 2; j >=i ; j--)
+        {
+            cout << j << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
+
+//         1
+//       2 3 2
+//     3 4 5 4 3
+//   4 5 6 7 6 5 4
+// 5 6 7 8 9 8 7 6 5
