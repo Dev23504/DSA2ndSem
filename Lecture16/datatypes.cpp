b76a0f445d23afbbbd850e7b045f56554e9837d5
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-03-09 10:48:54 +0530

    new

---
+++ Lecture16/datatypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    float b = 7.37;
+    double c = 9.87643;
+    char d = 'q';
+    bool e = true;
+    string s = "Dev";
+
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << s << endl;
+
+    // size pta krne ke
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(d) << endl;
+    cout << sizeof(e) << endl;
+    cout << sizeof(s) << endl;
+}
